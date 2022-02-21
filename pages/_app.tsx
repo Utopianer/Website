e791a1c636a4a917cac993bbeac4730e2bab9967
sasicodes <me@sasi.codes>
2022-02-21 20:25:50 +0530

    fix: remove padding issue

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,13 +8,13 @@ import { Toaster } from 'react-hot-toast'
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider attribute="class">
-      <Toaster position="top-right" />.
       <Script
         data-website-id="7fe34e1f-d06f-415b-98bc-c10004281128"
         async
         defer
         src="https://sudo-umami.up.railway.app/umami.js"
       />
+      <Toaster position="top-right" />
       <Component {...pageProps} />
     </ThemeProvider>
   )
