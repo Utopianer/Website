bd57dc4c36e847c1a49ad38eca546da1b721c8c6
Sasi <me@sasi.codes>
2022-02-27 16:43:38 +0530

    fix: code highlight by removing swc

--- next.config.js
+++ next.config.js
@@ -2,7 +2,6 @@
  * @type {import('next').NextConfig}
  */
 module.exports = {
-  swcMinify: true,
   reactStrictMode: true,
   images: {
     domains: ['cloudflare-ipfs.com', 'metadata.ens.domains']
